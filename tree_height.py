4c508d9517849875f9ac6bfb70bad7cd23dd39de
Arturs Kudeika <77011157+Kudern@users.noreply.github.com>
2023-06-01 15:31:28 +0300

    tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,43 +1,57 @@
-from collections import namedtuple
+import sys
+import threading
 
-Bracket = namedtuple("Bracket", ["char", "position"])
+# This class will help us to create a node
+class Node:
+    def __init__(self):
+        self.children = []
 
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
+# This function will help us to create a tree
+def create_tree(n, parents):
+    nodes = [Node() for _ in range(n)]
+    root = 0
+    for child_index in range(n):
+        parent_index = parents[child_index]
+        if parent_index == -1:
+            root = child_index
+        else:
+            nodes[parent_index].children.append(nodes[child_index])
+    return nodes[root]
 
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, add to stack
-            opening_brackets_stack.append(Bracket(next, i+1))
-
-        if next in ")]}":
-            # Process closing bracket, check if matches last opening bracket
-            if len(opening_brackets_stack) == 0:
-                # no opening bracket to match with
-                return i+1
-            last_opening_bracket = opening_brackets_stack.pop()
-            if not are_matching(last_opening_bracket.char, next):
-                # not a match with the last opening bracket
-                return i+1
-
-    # after processing all characters
-    if len(opening_brackets_stack) != 0:
-        # there are some unmatched opening brackets left
-        last_unmatched_opening_bracket = opening_brackets_stack[0]
-        return last_unmatched_opening_bracket.position
-    
-    # all brackets are properly matched
-    return "Success"
-
-def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    if mismatch == "Success":
-        print(mismatch)
+# This function will help us to compute the height of the tree
+def compute_height(node):
+    if not node.children:
+        return 1
     else:
-        print(mismatch)
+        return 1 + max(compute_height(child) for child in node.children)
 
-if __name__ == "__main__":
-    main()
+# This function will help us to read input data and run the solution
+def main():
+    # implement input form keyboard and from files
+    input_type = input("Input from File or Keyboard? (F/K): ")
+    if input_type.upper() == "F":
+        # let user input file name to use, don't allow file names with letter a
+        file_name = input("Enter file name (should not contain letter 'a'): ")
+        while 'a' in file_name:
+            file_name = input("Enter file name (should not contain letter 'a'): ")
+        with open(file_name) as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+    elif input_type.upper() == "K":
+        # input number of elements
+        # input values in one variable, separate with space, split these values in an array
+        n = int(input())
+        parents = list(map(int, input().split()))
+    else:
+        print("Invalid input type. Please enter F for file or K for keyboard input.")
+        return
+    # call the function and output it's result
+    tree = create_tree(n, parents)
+    print(compute_height(tree))
+
+# Increase the recursion limit and the stack size
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+
+# Run the solution in a new thread
+threading.Thread(target=main).start()
